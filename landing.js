ee38502a746326855a85ad951cf5aaead375d67a
Faith <fmugure@intracitygeeks.org>
2017-07-04 02:59:03 +0000

    bootstrap add

---
+++ landing.js
@@ -0,0 +1,41 @@
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"/>
+
+<!DOCTYPE html>
+<html>
+    <link type="text/css" rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lobster" />
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+    <head>
+        <title>Home</title>
+	</head>
+    
+
+<body>
+   
+   <div class="container-fluid">
+		<ul class="nav nav-pills">
+			    <li class="active"><a href="#">Home</a></li>
+				<li class="pull-right"><a href="#">The Leso</a></li>
+				<li class="pull-right"><a href="#">Leso Styles</a></li>
+				<li class="pull-right"><a href="#">Leso Facebook</a></li>
+				<li class="pull-right"><a href="#">Leso Contact</a></li>
+			</ul></div>
+			
+    
+    <h1>The Leso</h1>
+	  <div class="center">
+        <img class="smaller-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShUyvQ5HZdxPq7XRrqV2V0tnRC8xJtkgY_J0aToII3jZmB-ssZ">
+        <img class="smaller-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShUyvQ5HZdxPq7XRrqV2V0tnRC8xJtkgY_J0aToII3jZmB-ssZ">
+        <img class="smaller-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShUyvQ5HZdxPq7XRrqV2V0tnRC8xJtkgY_J0aToII3jZmB-ssZ">
+        <img class="smaller-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShUyvQ5HZdxPq7XRrqV2V0tnRC8xJtkgY_J0aToII3jZmB-ssZ">
+        <img class="smaller-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShUyvQ5HZdxPq7XRrqV2V0tnRC8xJtkgY_J0aToII3jZmB-ssZ">
+ </div>
+        
+	  
+		</div>
+				<br>
+				<img class="larger-image" src="https://s-media-cache-ak0.pinimg.com/736x/1a/c1/e8/1ac1e84e06a056a08537707e62f88951--african-fashion-style-nigerian-fashion.jpg">
+				
+				
+			</body>
+			</html>
